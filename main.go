84571a5966cadbc003423552a882b175579de376
Nikolai Kudriavtsev <n.a.kudriavtsev@gmail.com>
2023-02-19 01:15:33 +0500

    Simple REPL in main

--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
+func fatal(v ...any) {
+	fmt.Println(v...)
+	os.Exit(1)
+}
+
+func main() {
+	var operandString1, operandString2 string
+	var operatorRune int
+
+	for {
+		fmt.Println("Input:")
+		_, err := fmt.Scanf("%s %c %s\n", &operandString1, &operatorRune, &operandString2)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fatal(fmt.Errorf("bad input: %w", err))
+		}
+
+		fmt.Printf("Output:\n%#v %#v %#v\n", operandString1, operatorRune, operandString2)
+	}
+
+	fmt.Println("Exit")
 }
