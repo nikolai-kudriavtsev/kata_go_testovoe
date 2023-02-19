66119cfec0a75f2802bb213f28ed44feb63f4317
Nikolai Kudriavtsev <n.a.kudriavtsev@gmail.com>
2023-02-19 16:29:41 +0500

    Make intToRoman table external to the conversion

--- main.go
+++ main.go
@@ -175,27 +175,27 @@ func romanToInt(s string) (int, error) {
 	return sum, nil
 }
 
+var intToRomanTable = []struct {
+	value int
+	digit string
+}{
+	{1000, "M"},
+	{900, "CM"},
+	{500, "D"},
+	{400, "CD"},
+	{100, "C"},
+	{90, "XC"},
+	{50, "L"},
+	{40, "XL"},
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
 func intToRoman(number int) string {
 	var roman strings.Builder
-
-	intToRomanTable := []struct {
-		value int
-		digit string
-	}{
-		{1000, "M"},
-		{900, "CM"},
-		{500, "D"},
-		{400, "CD"},
-		{100, "C"},
-		{90, "XC"},
-		{50, "L"},
-		{40, "XL"},
-		{10, "X"},
-		{9, "IX"},
-		{5, "V"},
-		{4, "IV"},
-		{1, "I"},
-	}
 
 	for _, row := range intToRomanTable {
 		for number >= row.value {
